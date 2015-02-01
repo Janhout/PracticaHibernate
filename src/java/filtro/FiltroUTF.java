ac3501729577274df5b99b837d1569192ae64865
Rafael Gómez Rodríguez <rafagr8@gmail.com>
2015-02-01 20:47:31 +0100

    Añadido filtro para caracteres

---
+++ src/java/filtro/FiltroUTF.java
@@ -0,0 +1,39 @@
+package filtro;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter(filterName = "FiltroUTF", urlPatterns = {"/*"})
+public class FiltroUTF implements Filter {
+
+    private String encoding;
+
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+        encoding = config.getInitParameter("requestEncoding");
+        if (encoding == null) {
+            encoding = "UTF-8";
+        }
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain next) throws IOException, ServletException {
+        if (null == request.getCharacterEncoding()) {
+            request.setCharacterEncoding(encoding);
+        }
+        response.setContentType("text/html;  charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        next.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
